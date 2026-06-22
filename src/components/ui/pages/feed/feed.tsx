83281dc63c81352499ae38eda164939c61ccedd3
Alexander Kozhahin <korsukov2009@gmail.com>
2026-06-23 01:52:29 +0300

    все готвоо

--- src/components/ui/pages/feed/feed.tsx
+++ src/components/ui/pages/feed/feed.tsx
@@ -5,7 +5,7 @@ import styles from './feed.module.css';
 import { FeedUIProps } from './type';
 import { OrdersList, FeedInfo } from '@components';
 import { RefreshButton } from '@zlden/react-developer-burger-ui-components';
-
+// feed
 export const FeedUI: FC<FeedUIProps> = memo(({ orders, handleGetFeeds }) => (
   <main className={styles.containerMain}>
     <div className={`${styles.titleBox} mt-10 mb-5`}>
