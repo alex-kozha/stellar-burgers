280b042c4b732439a8d96ebf84f38e8c1fe0eae9
Alexander Kozhahin <korsukov2009@gmail.com>
2026-06-28 15:51:32 +0300

    финальная версия

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -154,7 +154,14 @@ const App = () => {
                 </ProtectedRoute>
               }
             />
-            <Route path='/profile/orders/:number' element={<OrderInfo />} />
+            <Route
+              path='/profile/orders/:number'
+              element={
+                <ProtectedRoute>
+                  <OrderInfo />
+                </ProtectedRoute>
+              }
+            />
 
             <Route path='*' element={<NotFound404 />} />
           </Routes>
